b843ffed0689b5e989247cec96899586fac4818b
OhDongI <odi0716@naver.com>
2025-03-30 20:58:20 +0900

    FEAT: React 활용하여 구조 잡기&스타일링

--- 미르-오동이/src/App.tsx
+++ 미르-오동이/src/App.tsx
@@ -3,9 +3,29 @@ import './App.css'
 function App() {
 
   return (
-    <>
-     <h1>Hello React</h1>
-    </>
+    <div className="todo-container">
+    <h1 className="todo-container__header">DONGI TODO</h1>
+    <form id="todo-form" className="todo-container__form">
+      <input
+        type="text"
+        id="todo-input"
+        className="todo-container__input"
+        placeholder="할 일 입력"
+        required
+      />
+      <button type="submit" className="todo-container__button">할 일 추가</button>
+    </form>
+    <div className="render-container">
+      <div className="render-container__section">
+        <h2 className="render-container__title">할 일</h2>
+        <ul id="todo-list" className="render-container__list"></ul>
+      </div>
+      <div className="render-container__section">
+        <h2 className="render-container__title">완료</h2>
+        <ul id="done-list" className="render-container__list"></ul>
+      </div>
+    </div>
+  </div>
   )
 }
 
