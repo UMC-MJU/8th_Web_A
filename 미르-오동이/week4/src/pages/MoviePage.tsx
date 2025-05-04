9a0e2310fd72f5e07aed7d6477026b3ae4e4bc74
OhDongI <odi0716@naver.com>
2025-05-04 23:14:50 +0900

    feat: useFetch 커스텀 훅 적용 및 로딩/에러 상태 처리 추가

--- 미르-오동이/week4/src/pages/MoviePage.tsx
+++ 미르-오동이/week4/src/pages/MoviePage.tsx
@@ -1,22 +1,14 @@
 import MovieCard from "../components/MovieCard";
-import useCustomFetch from "../hooks/useCustomFetch"; 
+import useFetch from "../hooks/useFetch";
 import { Movie } from "../types/movie";
 
-interface MovieApiResponse {
-  results: Movie[];
-}
-
 export default function MoviePage(): Element {
-  const {
-    data,
-    loading,
-    error
-  } = useCustomFetch<MovieApiResponse>(
+  const { data, loading, error } = useFetch<{ results: Movie[] }>(
     `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${import.meta.env.VITE_TMDB_KEY}`
   );
 
-  if (loading) return <div className="p-10 text-center">로딩 중...</div>;
-  if (error) return <div className="p-10 text-red-500 text-center">에러 발생: {error}</div>;
+  if (loading) return <div className="text-white p-10">로딩 중...</div>;
+  if (error) return <div className="text-red-500 p-10">에러 발생: {error.message}</div>;
 
   return (
     <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
